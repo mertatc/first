2f0c22c35da79e3578ca6d540c687ed513aa200d
mertatc <mertatc1903@hotmail.com>
2023-01-31 13:43:28 +0300

    a

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import './App.css';
 import UilReact from '@iconscout/react-unicons/icons/uil-react'
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+    //mx auto komutu ortalıyor
+    //mt- komutu yukardan boşluk bıraktırıyor
+    //py(padding) komutu genişlik kazandırıypr
+    return <div className="mx-auto max-w-screen-md mt-4 py-8 px-50
+    bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400"  ></div>
+
 }
 
 export default App;
---
+++ src/components/TopButtın.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function TopButtın() {
+  return (
+    <div>TopButtın</div>
+  )
+}
+
+export default TopButtın
