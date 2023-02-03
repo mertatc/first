75076fe78c344b1f29bb0ee115bf03ff9bb9d44a
mertatc <mertatc1903@hotmail.com>
2023-02-04 02:13:15 +0300

    a

--- src/components/Inputs.jsx
+++ src/components/Inputs.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import { UilAirplay } from '@iconscout/react-unicons'
+import { UilLocationPoint } from '@iconscout/react-unicons'
+
 
 function Inputs() {
   return (
-    <div>Inputs</div>
-  )
+    <div className="flex flex-row justify-center my-6">
+        <div className="flex flex-row w=2/4 items-center justify-center space-x-4">
+            <input 
+                type="text" 
+                placeholder='Search...'
+                className="text-xl font-light p-2 w-full shadow-xl focus:outline-none capitalize"/>
+        </div>
+    </div>
+  
+  ); 
 }
 
-export default Inputs
+export default Inputs;
