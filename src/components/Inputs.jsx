f0891e304ea6ac1ad41c4bd69c1100ea25a1c734
mertatc <mertatc1903@hotmail.com>
2023-02-14 13:42:21 +0300

    a

--- src/components/Inputs.jsx
+++ src/components/Inputs.jsx
@@ -12,8 +12,24 @@ function Inputs() {
                 placeholder='Search...' //texbox ın içinde saydam yazı
                 className="text-xl font-light p-2 w-full shadow-xl focus:outline-none capitalize"/>
             
-            <UilSearch size={25} className="text-white cursor-pointer"/>
-            <UilLocationPoint size={25} className="text-white cursor-pointer"/>
+            <UilSearch 
+            size={25} 
+            className="text-white cursor-pointer transition ease-out hover:scale-125"
+            />
+            <UilLocationPoint 
+            size={25} 
+            className="text-white cursor-pointer transition ease-out hover:scale-125"
+            />
+        </div>
+
+        <div className='flex flex-row w-1/4 items-center justify-center'>
+        <button name='metric' className='text-xl text-white font-light'>
+        °C
+        </button>
+        <p className='text-xl text-white mx-1'> / </p>
+        <button name='metric' className='text-xl text-white font-light'>
+        °F
+        </button>
         </div>
     </div>
   
