d4082a3138baa35a6a0c041505a6ab81a389191d
mertatc <mertatc1903@hotmail.com>
2023-02-14 13:23:45 +0300

    a

--- src/components/Inputs.jsx
+++ src/components/Inputs.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
-import { UilAirplay } from '@iconscout/react-unicons'
+import { UilSearch } from '@iconscout/react-unicons'
 import { UilLocationPoint } from '@iconscout/react-unicons'
 
 
 function Inputs() {
   return (
     <div className="flex flex-row justify-center my-6">
-        <div className="flex flex-row w=2/4 items-center justify-center space-x-4">
+        <div className="flex flex-row w=3/4 items-center justify-center space-x-4">
             <input 
                 type="text" 
-                placeholder='Search...'
+                placeholder='Search...' //texbox ın içinde saydam yazı
                 className="text-xl font-light p-2 w-full shadow-xl focus:outline-none capitalize"/>
+            
+            <UilSearch size={25} className="text-white cursor-pointer"/>
+            <UilLocationPoint size={25} className="text-white cursor-pointer"/>
         </div>
     </div>
   
